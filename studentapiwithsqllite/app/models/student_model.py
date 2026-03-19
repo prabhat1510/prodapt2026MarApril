802303e7fd6afbfe5fce3a0d37a3e6d99062b066
prabhat1510 <prabhatc@gmail.com>
2026-03-19 12:50:08 +0530

    Fixed Error Student API with SQLLite or DB

--- studentapiwithsqllite/app/models/student_model.py
+++ studentapiwithsqllite/app/models/student_model.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from app.db.database import Base
 
 class Student(Base):
-    __tablename__ = "users"
+    __tablename__ = "students"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
