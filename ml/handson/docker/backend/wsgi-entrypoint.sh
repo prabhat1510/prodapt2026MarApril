a7ef8f267a2e3e7e124b8c56207f686f250422f1
prabhat1510 <prabhatc@gmail.com>
2026-04-24 13:56:10 +0530

    Fixed Python related issue

--- ml/handson/docker/backend/wsgi-entrypoint.sh
+++ ml/handson/docker/backend/wsgi-entrypoint.sh
@@ -6,12 +6,12 @@ do
     echo "Waiting for server volume..."
 done
 
-until ./manage.py migrate
+until python manage.py migrate
 do
     echo "Waiting for database to be ready..."
     sleep 2
 done
 
-./manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
